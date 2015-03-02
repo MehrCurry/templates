5869a00623c7e84d88ae6bbf1174e0a6b1a273e5
Guido Zockoll <gzockoll@gmail.com>
2015-03-02 05:18:04 +0100

    banner

--- src/main/java/de/gzockoll/prototype/templates/ui/MainUI.java
+++ src/main/java/de/gzockoll/prototype/templates/ui/MainUI.java
@@ -1,14 +1,100 @@
 package de.gzockoll.prototype.templates.ui;
 
+import com.vaadin.annotations.Theme;
+import com.vaadin.data.Property;
+import com.vaadin.navigator.Navigator;
+import com.vaadin.server.FontAwesome;
+import com.vaadin.server.Responsive;
 import com.vaadin.server.VaadinRequest;
-import com.vaadin.ui.Label;
-import com.vaadin.ui.UI;
+import com.vaadin.ui.*;
 import org.vaadin.spring.annotation.VaadinUI;
 
+import java.util.Arrays;
+import java.util.LinkedHashMap;
+
 @VaadinUI
+@Theme("valo")
 public class MainUI extends UI {
-    @Override
-    protected void init(VaadinRequest vaadinRequest) {
-        setContent(new Label("Hi!"));
+
+    ValoMenuLayout root = new ValoMenuLayout();
+    ComponentContainer viewDisplay = root.getContentContainer();
+    CssLayout menu = new CssLayout();
+    CssLayout menuItemsLayout = new CssLayout();
+    {
+        menu.setId("testMenu");
     }
+    private Navigator navigator;
+    private final LinkedHashMap<String, String> menuItems = new LinkedHashMap<String, String>();
+    private boolean testMode;
+
+    @Override
+    protected void init(VaadinRequest request) {
+        if (request.getParameter("test") != null) {
+            testMode = true;
+
+            if (browserCantRenderFontsConsistently()) {
+                getPage().getStyles().add(
+                        ".v-app.v-app.v-app {font-family: Sans-Serif;}");
+            }
+        }
+        if (getPage().getWebBrowser().isIE()
+                && getPage().getWebBrowser().getBrowserMajorVersion() == 9) {
+            menu.setWidth("320px");
+        }
+        if (!testMode) {
+            Responsive.makeResponsive(this);
+        }
+
+        getPage().setTitle("Valo Theme Test");
+        setContent(root);
+        root.setWidth("100%");
+
+        root.addMenu(buildMenu());
+        viewDisplay.addComponent(getContentComponents());
+    }
+
+    private Component buildMenu() {
+        final CssLayout menu = new CssLayout();
+        menu.addStyleName("large-icons");
+
+        final Label logo = new Label("Va");
+        logo.setSizeUndefined();
+        logo.setPrimaryStyleName("valo-menu-logo");
+        menu.addComponent(logo);
+
+        Button b = new Button(
+                "Reference <span class=\"valo-menu-badge\">3</span>");
+        b.setIcon(FontAwesome.FILE_CODE_O);
+        b.setPrimaryStyleName("valo-menu-item");
+        b.addStyleName("selected");
+        b.setHtmlContentAllowed(true);
+        menu.addComponent(b);
+
+        b = new Button("API");
+        b.setIcon(FontAwesome.FILE_PDF_O);
+        b.setPrimaryStyleName("valo-menu-item");
+        menu.addComponent(b);
+
+        return menu;
+    }
+
+    private VerticalLayout getContentComponents() {
+        VerticalLayout layout=new VerticalLayout();
+        layout.addComponent(new Label("Hi!"));
+        return layout;
+    }
+
+    private boolean browserCantRenderFontsConsistently() {
+        // PhantomJS renders font correctly about 50% of the time, so
+        // disable it to have consistent screenshots
+        // https://github.com/ariya/phantomjs/issues/10592
+
+        // IE8 also has randomness in its font rendering...
+
+        return getPage().getWebBrowser().getBrowserApplication()
+                .contains("PhantomJS")
+                || (getPage().getWebBrowser().isIE() && getPage()
+                .getWebBrowser().getBrowserMajorVersion() <= 9);
+    }
+
 }
